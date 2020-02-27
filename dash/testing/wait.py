cbf88a1885f392cd9e9e8aba775a9dae70eaec74
Jody McIntyre <scjody@modernduck.com>
2020-02-27 18:41:03 -0500

    Adapt existing wait classes to exception-based system

--- dash/testing/wait.py
+++ dash/testing/wait.py
@@ -71,56 +71,50 @@ def until_not(
 
 
 class contains_text(object):
+    false_exceptions = (WebDriverException,)
+
     def __init__(self, selector, text):
         self.selector = selector
         self.text = text
 
     def __call__(self, driver):
-        try:
-            elem = driver.find_element_by_css_selector(self.selector)
-            logger.debug(
-                "contains text {%s} => expected %s", elem.text, self.text
-            )
-            return self.text in str(elem.text) or self.text in str(
-                elem.get_attribute("value")
-            )
-        except WebDriverException:
-            logger.exception("contains_text encountered an exception")
-            return False
+        elem = driver.find_element_by_css_selector(self.selector)
+        logger.debug(
+            "contains text {%s} => expected %s", elem.text, self.text
+        )
+        return self.text in str(elem.text) or self.text in str(
+            elem.get_attribute("value")
+        )
 
 
 class text_to_equal(object):
+    false_exceptions = (WebDriverException,)
+
     def __init__(self, selector, text):
         self.selector = selector
         self.text = text
 
     def __call__(self, driver):
-        try:
-            elem = driver.find_element_by_css_selector(self.selector)
-            logger.debug(
-                "text to equal {%s} => expected %s", elem.text, self.text
-            )
-            return (
-                str(elem.text) == self.text
-                or str(elem.get_attribute("value")) == self.text
-            )
-        except WebDriverException:
-            logger.exception("text_to_equal encountered an exception")
-            return False
+        elem = driver.find_element_by_css_selector(self.selector)
+        logger.debug(
+            "text to equal {%s} => expected %s", elem.text, self.text
+        )
+        return (
+            str(elem.text) == self.text
+            or str(elem.get_attribute("value")) == self.text
+        )
 
 
 class style_to_equal(object):
+    false_exceptions = (WebDriverException,)
+
     def __init__(self, selector, style, val):
         self.selector = selector
         self.style = style
         self.val = val
 
     def __call__(self, driver):
-        try:
-            elem = driver.find_element_by_css_selector(self.selector)
-            val = elem.value_of_css_property(self.style)
-            logger.debug("style to equal {%s} => expected %s", val, self.val)
-            return val == self.val
-        except WebDriverException:
-            logger.exception("style_to_equal encountered an exception")
-            return False
+        elem = driver.find_element_by_css_selector(self.selector)
+        val = elem.value_of_css_property(self.style)
+        logger.debug("style to equal {%s} => expected %s", val, self.val)
+        return val == self.val
